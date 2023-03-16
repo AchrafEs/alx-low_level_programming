30cb63f74abfeb56ec88eab94e99e159e79bd360
AchrafEs <achraf.es2110@gmail.com>
2023-03-16 16:06:07 -0700

    task 4 hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - A programme that prints a line using the puts function
+ * main - a programme that prints a line using the puts function
  * Return : 0 (success)
  */
 int main(void)
