570ffadca70736f1cb938dfdec085a5cc9b53dbb
AchrafEs <achraf.es2110@gmail.com>
2023-03-17 00:32:30 -0700

    modified 6-size.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 /**
- * main - A programme that prints a line using the puts function
- * Description : Prints out a string to stdout
- * Return : Always 0 (success)
+ * main - Entry point
+ *
+ * return: always 0 (success)
  */
+
 int main(void)
 {
+
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - a C program that prints the size of various
- * types on the computer it is compiled and run on.
- * return: always 0 (success)
+ * main - Entry point
+ * return: always 0 if the programe executed successfully
  */
 int main(void)
 {
