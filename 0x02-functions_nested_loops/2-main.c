06fe7067e976e39316d03e31f50c190ced0dd654
AchrafEs <achraf.es2110@gmail.com>
2023-03-21 16:59:41 -0700

    task 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - A program that prints
+ * the alphabet 10 times.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
