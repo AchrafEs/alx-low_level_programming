432a8969b4407035abd09d5853161693f42d7f5f
AchrafEs <achraf.es2110@gmail.com>
2023-03-25 07:50:05 -0700

    101-print_numbers

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_number - a function that prints an integer.
+ * @n: the int to be printed
+ *
+ * Return: the int n.
+ */
+void print_number(int n)
+{
+	unsigned int i = n;
+
+	if (n < 0)
+	{
+		n *= -1;
+		i = n;
+		putchar('-');
+	}
+	i /= 10;
+	if (i != 0)
+		print_number(i);
+	putchar((unsigned int) n % 10 + '0');
+}
