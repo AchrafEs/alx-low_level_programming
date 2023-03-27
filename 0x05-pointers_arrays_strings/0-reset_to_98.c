30386db1f534e046495f8044b847405db101701e
AchrafEs <achraf.es2110@gmail.com>
2023-03-27 07:00:43 -0700

    0-reset_to_98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 /**
  * reset_to_98 - A function that takes a pointer to an int as parameter
  * and updates the value it points to to 98.
- * @*n: the pointer
+ * @n: the pointer
  *
  * Return: nothing
  */
