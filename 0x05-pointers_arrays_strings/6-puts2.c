88ccf693a2b0163e54d23c7fa06d6c024e0dbe8f
AchrafEs <achraf.es2110@gmail.com>
2023-03-27 15:39:28 -0700

    task 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,9 +10,13 @@
  */
 void puts2(char *str)
 {
-	int i;
+	int i, j = 0;
 
-	for (i = 0; str[i] != '\0'; i += 2)
+	while (str[j] != '\0')
+	{
+		j++;
+	}
+	for (i = 0; i < j; i += 2)
 	{
 		printf("%c", str[i]);
 	}
