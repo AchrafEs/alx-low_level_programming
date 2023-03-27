6f59987250beb1c8178c485352d11d54798b1e10
AchrafEs <achraf.es2110@gmail.com>
2023-03-27 09:45:08 -0700

    print in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_rev - A function that prints a string, in reverse, followed by a new line.
+ * @s: the char to be checked
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int len = strlen(s);
+	int i;
+
+	for (i = len - 1; i >= 0; i--)
+	{
+		printf("%c", s[i]);
+	}
+	putchar('\n');
+}
