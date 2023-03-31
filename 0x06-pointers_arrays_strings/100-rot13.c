bab03880261d1a8ab2a792a249a639e26311e0ec
AchrafEs <achraf.es2110@gmail.com>
2023-03-31 07:02:23 -0700

    added rot13 script

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *rot13 - a function that encodes a string using rot13.
+ * @str: the string
+ *
+ * Return: the encoded string
+ */
+char *rot13(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = (char) ((str[i] - 'a' + 13) % 26 + 'a');
+		}
+		else if (str[i] >= 'A' && str[i] <= 'Z')
+		{
+			str[i] = (char) ((str[i] - 'A' + 13) % 26 + 'A');
+		}
+	}
+	return (str);
+}
---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_number - A function that prints an integer.
+ * @n: the integer to be printed
+ *
+ * Return: void.
+ */
+void print_number(int n)
+{
+	struct result{ int value;
+	}
+	result -> value = n;
+}
