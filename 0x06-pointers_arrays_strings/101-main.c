aaef24b6312ad84b453eeae9242761562470deb2
AchrafEs <achraf.es2110@gmail.com>
2023-04-02 15:05:23 +0000

    modified the test file 101-main.c and the funtion print_numbers

--- 0x06-pointers_arrays_strings/101-main.c
+++ 0x06-pointers_arrays_strings/101-main.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <limits.h>
 
 /**
  * main - check the code
@@ -8,15 +9,23 @@
  */
 int main(void)
 {
-	print_number(98);
+	print_number(0);
 	putchar('\n');
-	print_number(402);
+	print_number(3);
+	putchar('\n');
+	print_number(-1);
+	putchar('\n');
+	print_number(10);
+	putchar('\n');
+	print_number(-99);
 	putchar('\n');
 	print_number(1024);
 	putchar('\n');
-	print_number(0);
+	print_number(-4096);
 	putchar('\n');
-	print_number(-98);
+	print_number(INT_MAX);
+	putchar('\n');
+	print_number(INT_MIN);
 	putchar('\n');
 	return (0);
 }
