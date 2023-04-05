a16be37f0a874340b837cc4cb5748df53afd4ef0
AchrafEs <achraf.es2110@gmail.com>
2023-04-05 15:09:17 +0000

    fixed betty style issue

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -2,7 +2,8 @@
 #include <math.h>
 
 /**
- * _pow_recursion - a function that returns the value of x raised to the power of y.
+ * _pow_recursion - a function that returns the value
+ * of x raised to the power of y.
  * @x: the int to return
  * @y: the int the power.
  *
@@ -20,6 +21,6 @@ int _pow_recursion(int x, int y)
 	}
 	else
 	{
-		return (x * _pow_recursion(x, y-1));
+		return (x * _pow_recursion(x, y - 1));
 	}
 }
