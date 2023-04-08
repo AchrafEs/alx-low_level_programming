80094ac9ada2d4a954152a9bab1cb0ee3f9e6a45
AchrafEs <achraf.es2110@gmail.com>
2023-04-08 22:01:10 +0000

    added 0-whatsmyname.c along with the header file main.h

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - A function that prints its name.
+ * @argc: argument count.
+ * @argv: argument vector.
+ *
+ * Return: Always 0 (success).
+ */
+int main(int argc, char **argv)
+{
+	(void) argc;
+	
+	puts(*argv);
+	return (0);
+}
