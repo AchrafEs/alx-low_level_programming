8b439bd0e63b7cc6f56249e5f33bf4a7acddafa8
AchrafEs <achraf.es2110@gmail.com>
2023-04-11 16:02:02 +0000

    added the files 0-create_array.c 0-main.c README.md main.h

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - A function that creates an array of chars
+ * and initializes it with a specific char.
+ * @size: size of the array
+ * @c: the character to create the array from
+ *
+ * Return: a pointer to char.
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *str;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	i = 0;
+	str = malloc(size * sizeof(char));
+	while (i < size)
+	{
+		str[i] = c;
+		i++;
+	}
+	return (str);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,8 @@
+In this directory we'll be learning about:
+
+* Malloc Function.
+* free function.
+
+Malloc : is a function that allows you to dynamically allocate memory for a variable during runtime. You tell the function how much memory you need, and it returns a pointer to the first byte of that memory block.
+free   : is a function that releases the memory back to the operating system.
+
