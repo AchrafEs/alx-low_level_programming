cf2ae4c121dd426e06856aa962dfac243e2bf833
AchrafEs <achraf.es2110@gmail.com>
2023-04-12 14:23:58 +0000

    hot fix

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -25,7 +25,7 @@ char *argstostr(int ac, char **av)
 	for (i = 0; i < ac; i++)
 	{
 		printf("%s\n", av[i]);
-		len += strlen(av[i]);
+		len += strlen(av[i]) + 1;
 	}
 	str = malloc((len + 1) * sizeof(char));
 	if (str == NULL)
