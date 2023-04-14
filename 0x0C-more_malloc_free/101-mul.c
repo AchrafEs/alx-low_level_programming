9715fcc1601cb9cdc7ff416299500d2a5803a50b
AchrafEs <achraf.es2110@gmail.com>
2023-04-14 00:53:09 +0000

    modified the file 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,34 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
+#include <string.h>
 
-/**
- * main - A program that multiplies two positive numbers.
- * @argc: argument count.
- * @argv: argument vector.
- *
- * Return: Always 0 (success).
- */
+#define MAX_DIGITS 1000
+
+void multiply(char *num1, char *num2, char *result);
+
 int main(int argc, char **argv)
 {
-	int num1, num2, mul, i;
+	char *num1, *num2, *result;
+	int len1, len2, i;
 
-	if (argc < 3)
+	if (argc != 3)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	for (i = 1; i < argc; i++)
+	num1 = argv[1];
+	num2 = argv[2];
+	len1 = strlen(num1);
+	len2 = strlen(num2);
+	result = calloc(len1 + len2, sizeof(char));
+	if (result == NULL)
 	{
-		if (!isdigit(*argv[i]))
+		printf("Error: Memory allocation failed\n");
+		return (1);
+	}
+	multiply(num1, num2, result);
+	i = 0;
+	while (result[i] == '0' && i < len1 + len2 - 1)
+	{
+		i++;
+	}
+	printf("%s\n", &result[i]);
+	free(result);
+	return (0);
+}
+
+void multiply(char *num1, char *num2, char *result)
+{
+	int len1 = strlen(num1);
+	int len2 = strlen(num2);
+	int i, j;
+	int digit1, digit2, product, pos1, pos2, sum;
+
+	for (i = 0; i < len1 + len2; i++)
+	{
+		result[i] = '0';
+	}
+	for (i = len1 - 1; i >= 0; i--)
+	{
+		for (j = len2 - 1; j >= 0; j--)
 		{
-			printf("Error\n");
-			exit(98);
+			digit1 = num1[i] - '0';
+			digit2 = num2[j] - '0';
+			product = digit1 * digit2;
+			pos1 = i + j;
+			pos2 = i + j + 1;
+			sum = (result[pos2] - '0') + product;
+			result[pos2] = (sum % 10) + '0';
+			result[pos1] += sum / 10;
 		}
 	}
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
-	mul = num1 * num2;
-	printf("%d\n", mul);
-	return (0);
 }
