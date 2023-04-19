57b84170ffc2f7c427aff889821667f5e264724f
AchrafEs <achraf.es2110@gmail.com>
2023-04-19 15:05:57 +0000

    modified the file 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,23 +1,4 @@
 #include "function_pointers.h"
-#include <stdio.h>
-
-/**
- * print - function that prints a string.
- * @str: the string to print
- *
- * Return: void.
- */
-
-void print(char *str)
-{
-	int i = 0;
-
-	while (str[i])
-	{
-		putchar(str[i]);
-		i++;
-	}
-}
 /**
  * print_name - A function that prints a name
  * @name: the name to print.
