4d938b9a093d8d0f501b74e913476e54c83242a0
AchrafEs <achraf.es2110@gmail.com>
2023-04-20 01:27:56 +0000

    added the program that performs simple operations.

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,41 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - A program that performs simple operations.
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char **argv)
+{
+	int num1, num2, len;
+	int calc = 0;
+	int (*operator)(int, int);
+
+	if (argc < 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	len = strlen(argv[2]);
+	operator = get_op_func(argv[2]);
+	if (len != 1 || operator == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if (num2 == 0 && (strcmp(argv[3], "/") == 0 || strcmp(argv[3], "%") == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	calc = operator(num1, num2);
+	printf("%d\n", calc);
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,65 @@
+#include "3-calc.h"
+
+/**
+ * op_add - A function that calculates the sum of two
+ * variables a & b.
+ * @a: the first variable.
+ * @b: the second variable.
+ *
+ * Return: the sum.
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - A function that calculates the difference of
+ * two variables a & b.
+ * @a: the first variable
+ * @b: the second variable
+ *
+ * Return: the difference.
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - A function that calculates the mul of two
+ * variables a & b
+ * @a: the first variable.
+ * @b: the second variable.
+ *
+ * Return: the mul
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - A function that calculates the division
+ * of two variables a & b.
+ * @a: the first variable.
+ * @b: the second variable.
+ *
+ * Return: the division.
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - A function that calculates the modulo of two vars
+ * @a: the first variable
+ * @b: the second variable
+ *
+ * Return: the result
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
