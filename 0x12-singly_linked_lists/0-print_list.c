2b7edc59abdd5e0de5c35285f673ff8461f62cc2
AchrafEs <achraf.es2110@gmail.com>
2023-04-27 14:31:16 +0000

    updated the function print_list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,7 +3,6 @@
 
 /**
  * print_list - a function that prints all the elements of a list_t list.
- * @list_t: the struct
  * @h: an index
  *
  * Return: the number of nodes.
@@ -15,6 +14,7 @@ size_t print_list(const list_t *h)
 	if (h == NULL)
 	{
 		printf("[0] (nil)\n");
+		return (0);
 	}
 	while (h != NULL)
 	{
