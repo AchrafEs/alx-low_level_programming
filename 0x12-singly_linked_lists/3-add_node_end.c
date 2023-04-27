38ca46a8e7c9e945847d6fe6443e4d28772f4521
AchrafEs <achraf.es2110@gmail.com>
2023-04-27 15:55:42 +0000

    modified the function add_node_end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -7,6 +7,8 @@
  * at the end of a list_t list.
  * @head: A double pointer to the head of the list_t
  * @str: the string to be added to the new node.
+ *
+ * Return: the address of the new element or NULL if it failed
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
