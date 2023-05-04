515adbed186f03f489ccec4b26bb8e52035389d8
AchrafEs <achraf.es2110@gmail.com>
2023-05-04 14:41:15 +0000

    modified the function

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,7 +12,7 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int count = 0;
 	unsigned int value = 0;
 
-	while (*b != '\0')
+	while (*b)
 	{
 		if ((*b != '0' && *b != '1') || b == NULL)
 		{
