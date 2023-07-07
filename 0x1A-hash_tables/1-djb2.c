b37802c3a1c670a6b44aaa1add317a144bdfad9d
AchrafEs <achraf.es2110@gmail.com>
2023-07-07 14:17:22 +0000

    updated the file 1-djb2.c

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -2,9 +2,9 @@
 
 /**
  * hash_djb2 - a hash function implementing the djb2 algorithm
- * @str: a string
+ * @str: string used to generate hash value
  *
- * Return: djb2 algorithm.
+ * Return: hash value
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
